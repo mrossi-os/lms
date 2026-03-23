d231df9da1403031e01292dd1a20451692c4878b
Gabriele Pagnotta <gabriele@overside.it>
2026-03-23 19:01:51 +0100

    fix bug smtp session closed before sending email

--- apps/os_lms/os_lms/__init__.py
+++ apps/os_lms/os_lms/__init__.py
@@ -1,1 +1,4 @@
 __version__ = "0.0.1"
+
+# Monkey-patch SMTPServer.session property
+import os_lms.overrides.smtp  # noqa: F401
--- apps/os_lms/os_lms/overrides/email_account.py
+++ apps/os_lms/os_lms/overrides/email_account.py
@@ -1,12 +1,12 @@
 from frappe.email.doctype.email_account.email_account import EmailAccount
 import os
+import frappe
 
 
 class CustomEmailAccount(EmailAccount):
     def get_smtp_server(self):
         server = super().get_smtp_server()
         if server and server.session:
-            if(server.session.esmtp_features["size"] == "0"):
+            if server.session.esmtp_features["size"] == "0":
                 server.session.esmtp_features["size"] = "1000000"
-        print("OKKKKK")
         return server
